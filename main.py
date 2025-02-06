28048dbbfaa085d2d6c767b177c7f0e617fcff00
designed-to-make <ben@designedtomake.com>
2025-02-06 13:03:21 +0000

    initial upload of files
    includes ota update

---
+++ main.py
@@ -0,0 +1,73 @@
+#Main program to run at boot
+#28/01/2025
+#V0.01
+
+
+from machine import Pin, I2C, PWM
+from time import sleep, sleep_ms
+from bme680 import *
+from ina260 import INA260, AveragingCount, ConversionTime
+from seascooter import *
+
+# RPi Pico - Pin assignment
+i2c = I2C(0, scl=Pin(17), sda=Pin(16),freq=400000)
+
+#reed switch pins
+p_aux = Pin(27, Pin.IN, Pin.PULL_DOWN)
+p_0 = Pin(26, Pin.IN, Pin.PULL_DOWN)
+p_50 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+p_100 = Pin(19, Pin.IN, Pin.PULL_DOWN)
+
+#Motor control pins
+m_pwm = PWM(Pin(12))
+m_forwards = Pin(11,Pin.OUT)
+m_backwards = Pin(10,Pin.OUT)
+
+#Intialise motor
+m_pwm.freq(10000)
+m_forwards.value(1)
+m_backwards.value(0)
+m_pwm.duty_u16(0)
+
+#Add a short delay to wait until the I2C port has finished activating.
+sleep_ms(100)
+#intialise the BME680 sensor
+bme = BME680_I2C(i2c=i2c)
+#intialise the INA260 Sensor 
+ob_ina260 = INA260(i2c)
+
+#intilise variables
+pressure 		= 0
+temp			= 0
+humid			= 0
+current			= 0
+voltage			= 0
+control_state	= 0
+lts				= 0
+
+#measurements
+
+    
+
+while True:
+    pressure 		= bme.pressure # hPa
+    temp	 		= bme.temperature # degrees 
+    humid			= bme.humidity # %
+    current			= av_current(ob_ina260, 10)/1000 #Amps
+    voltage			= ob_ina260.voltage # Volts
+    control_value 	= reed_speed_control(p_aux,p_0,p_50,p_100)
+    
+    if control_value >=0:
+        m_pwm.duty_u16(3*control_value)
+        
+    else:
+        m_pwm.duty_u16(0)
+        #####write auxillary function to call####
+    
+    lts = log_state(lts,pressure, temp, humid, current, voltage, control_value)
+    sleep_ms(100)
+    print(m_pwm.duty_u16(),control_value)
+    
+    
+            
+    
---
+++ seascooter.py
@@ -0,0 +1,69 @@
+#this module includes all functions that light be required to log data from sensors.
+
+from time import sleep, time
+
+
+# Check that the current not negative and a buffer overrun number around 82000 if so make negative else pass raw value.
+def real_current(raw_reading):
+    if raw_reading > 45000:
+        actual_current = raw_reading - 81920
+    else:
+        actual_current = raw_reading
+    return actual_current
+
+
+def av_current(ina_obj, samples):
+    sample_number = 1
+    readings =[]
+    while sample_number < samples+1:
+        raw_reading =  ina_obj.current
+        reading = real_current(raw_reading)
+        readings.append(reading)
+        sample_number = sample_number+1
+        sleep (0.001)
+    else:
+        return sum(readings)/(samples)
+
+
+def log_state(last_time_stamp,pressure, temp, humid, current, voltage, control_state):
+        log_time = time()
+        if last_time_stamp+5 < log_time:
+            last_time_stamp = log_time
+            r_press	= str(round(pressure,1)) 		# in hPa
+            r_temp 	= str(round(temp, 1)) 			# in Degress C
+            r_humid	= str(round(humid,1)) 			# in %
+            r_curr	= str(round(current,3)) 	# in Amps
+            r_volt	= str(round(voltage,3))			# in Volts
+            r_cont	= str(round(control_state,0))	# in throttle % if +ve else if -ve = Auxillary switch activated
+            
+            values_to_log = [log_time, r_press, r_temp, r_humid, r_curr, r_volt, r_cont]
+            
+            str_vtl = str(values_to_log)
+            f = open("scoot_log.txt", "a")
+            f.write(str_vtl)
+            f.close()
+            last_time_stamp = log_time
+        else:
+            print('skipped logging')
+              
+        return last_time_stamp
+
+def reed_speed_control(Pin_Aux,P_20,P_60,P_100):
+    state = [Pin_Aux.value(),P_20.value(),P_60.value(),P_100.value()]
+    state_values = [-10,0,50,100]
+    if sum(state) == 0:
+        signal = 0
+    else:
+        signal = dot_product(state,state_values)/(sum(state))
+    return int(round(signal,0))
+    
+def dot_product(v1, v2):
+    """
+    Computes the dot product of two vectors of equal length.
+    """
+    if len(v1) != len(v2):
+        raise ValueError("Vectors must have the same length.")
+    result = 0
+    for i in range(len(v1)):
+        result += v1[i] * v2[i]
+    return result
